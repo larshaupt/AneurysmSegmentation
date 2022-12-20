f749358ae333e38a5d9ab37deaa41f3f87f6d8bb
larshaupt <lhauptmann@ethz.ch>
2022-12-20 23:27:37 +0100

    removed pdb

--- train/options.py
+++ train/options.py
@@ -116,7 +116,7 @@ class Options:
             print(e)
 
     def parse_arguments(self):
-        pdb.set_trace()
+
         self.parser = argparse.ArgumentParser(description="Script for training")
         self.parser.add_argument("-ex", "--exp_path", type=str, default='', help="Path to experiment config file")
 
@@ -206,7 +206,7 @@ class Options:
         self.opt.experiment_name = '%s_sweep_%d'%(self.opt.dataset, timestamp)
         self.opt.experiment_group_name = self.opt.experiment_name
         self.opt.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        pdb.set_trace()
+
         if self.opt.path_data == "":
             self.opt.path_data = '/usr/bmicnas01/data-biwi-01/bmicdatasets/Processed/USZ_BrainArtery/%s/data/'%(self.opt.dataset)
         if self.opt.path_split == "":
